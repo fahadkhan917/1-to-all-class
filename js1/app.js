e59cc5e6a74f8a50a1a1791816f956d78aa6488d
fahadkhan917 <fk16181@gmail.com>
2021-05-04 16:56:30 +0500

    Update app.js

--- js1/app.js
+++ js1/app.js
@@ -432,3 +432,32 @@
 // document.write(are+"<br>")
 
 
+
+
+// The Lifetime Supply Calculator: Ever wonder how
+// much a “lifetime supply” of your favorite snack is?
+// Wonder no more.
+// a. Store your favorite snack into a variable
+// b. Store your current age into a variable.
+// c. Store a maximum age into a variable.
+// d. Store an estimated amount per day (as a number).
+// e. Calculate how many would you eat total for the rest of
+// your life.
+// Output the result to the screen like so: “You will need
+// NNNN to last you until the ripe old age of NN”
+
+
+
+// var fav_snack="chocolate chips"
+// var age=15;
+// var maxage=65;
+// var snackperday=3;
+
+// document.write(fav_snack+"<br>")
+// document.write(age+"<br>")
+// document.write(maxage+"<br>")
+// document.write(snackperday+"<br>")
+
+
+
+
