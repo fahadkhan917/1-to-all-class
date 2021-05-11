e12346bc05926d111946b1f851e7a8bb131ffc8f
fahadkhan917 <fk16181@gmail.com>
2021-05-11 20:17:44 +0500

    Update app.js

--- js1/app.js
+++ js1/app.js
@@ -979,3 +979,32 @@
 //                 document.write("u")
                 
 //                 }
+
+
+
+// Write a program that
+// a. Store correct password in a JS variable.
+// b. Asks user to enter his/her password
+// c. Validate the two passwords:
+// i. Check if user has entered password. If not, then
+// give message “ Please enter your password”
+// ii. Check if both passwords are same. If they are
+// same, show message “Correct! The password you
+// entered matches the original password”. Show
+// “Incorrect password” otherwise
+
+
+
+// var pass="fahad";
+// var user=prompt("enter the password");
+// if(pass==user){
+
+// document.write("correct")
+
+// }
+
+// else{
+
+// document.write("please enter your password")
+
+// }
