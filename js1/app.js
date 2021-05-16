0c49daff5a1752db1530594efc887014a07e7b29
fahadkhan917 <fk16181@gmail.com>
2021-05-16 18:14:57 +0500

    Update app.js

--- js1/app.js
+++ js1/app.js
@@ -1156,3 +1156,38 @@
 // document.write("Score of"+stu[0]+"is"+sc[0]+". Percentage:"+sc[0]/tot*100+"<br>")
 // document.write("Score of"+stu[1]+"is"+sc[1]+". Percentage:"+sc[1]/tot*100+"<br>")
 // document.write("Score of"+stu[2]+"is"+sc[2]+". Percentage:"+sc[2]/tot*100+"<br>")
+
+
+
+// . Initialize an array with color names. Display the array
+// // elements in your browser
+// a. Ask the user what color he/she wants to add to the
+// beginning & add that color to the beginning of the array.
+// Display the updated array in your browser.
+// b. Ask the user what color he/she wants to add to the end
+// & add that color to the end of the array. Display the
+// updated array in your browser.
+// c. Add two more color to the beginning of the array.
+// Display the updated array in your browser.
+// d. Delete the first color in the array. Display the updated
+// array in your browser.
+// e. Delete the last color in the array. Display the updated
+// array in your browser.
+// f. Ask the user at which index he/she wants to add a color
+// & color name. Then add the color to desired
+// position/index. . Display the updated array in your
+// browser.
+// g. Ask the user at which index he/she wants to delete
+// color(s) & how many colors he/she wants to delete. Then
+// remove the same number of color(s) from user-defined
+// position/index. . Display the updated array in your
+// browser.
+
+var colo=["red","green","blue"]
+document.write(colo+"<br>");
+var nead=prompt("enter the user")
+colo.unshift(nead)
+document.write(colo+"<br>")
+var nead1=prompt("enter the user")
+colo.push(nead1)
+document.write(colo+"<br>")
