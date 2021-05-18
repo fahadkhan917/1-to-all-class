9dd9a9e0a20f6dd808d64176c2620d68e73176e0
fahadkhan917 <fk16181@gmail.com>
2021-05-19 04:00:42 +0500

    update

---
+++ js2/app.js
@@ -0,0 +1,15 @@
+// Write a program to store student scores in an array &
+// sort the array in ascending order using Array’s sort
+// method.
+
+
+// var arr=[320,230,480,120]
+// document.write(arr+"<br>")
+// document.write(arr.sort())
+
+// Write a program to initialize an array with city names.
+// Copy 3 array elements from cities array to selectedCities
+// array
+
+var city=["karachi","lahore","Islamabad","Quetta","Peshawar"];
+document.write(city)
