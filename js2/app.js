4843ac58ec45c1dfbb016f4dcae83f13b115a6c4
fahadkhan917 <fk16181@gmail.com>
2021-07-30 20:28:39 +0500

    Update app.js

--- js2/app.js
+++ js2/app.js
@@ -1304,3 +1304,48 @@
 // For example, in the sentence
 
 
+var a=prompt("enter the string")
+var found=0;
+function isvowel(ch){
+
+switch(ch){
+
+case 'A':
+case 'a':
+case 'E':
+case 'e':
+case 'I':
+case 'i':
+case 'O':
+case 'o':
+case 'U':
+case 'u':
+    return true;
+    
+    default:
+        return false;
+
+
+
+}
+
+
+}
+
+for(i=0;i<a.length;i++){
+if(isvowel(a[i])){
+
+found++;
+
+}
+else{
+
+found=0;
+
+}
+if(found==2){
+
+document.write(a[i-1], a[i]+"<br>")
+
+}
+}
