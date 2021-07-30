1ecb4d53400363da29121549ef4ca883c26d7af2
fahadkhan917 <fk16181@gmail.com>
2021-07-30 22:15:45 +0500

    Update app.js

--- js2/app.js
+++ js2/app.js
@@ -1384,3 +1384,17 @@
 // feet(dis)
 // inch(dis)
 // centi(dis)
+
+
+
+// . A cashier has currency notes of denominations 10, 50 and
+// 100. If the amount to be withdrawn is input through the
+// keyboard in hundreds, find the total number of currency notes
+// of each denomination the cashier will have to give to the
+// withdrawer
+
+// var a=parseInt(prompt("enter the amount"));
+// document.write("the total 100 notes"+(a/100))
+// document.write("the total 50 notes"+(a%100)/50)
+// document.write("the total 10 notes"+((a%100)%50)/10)
+
