3b3894f3cbcaea8595426bc28afaeb07076116f0
fahadkhan917 <fk16181@gmail.com>
2021-07-30 16:24:50 +0500

    Update app.js

--- js2/app.js
+++ js2/app.js
@@ -1126,3 +1126,29 @@
 
 // abc("JSResourceS.com",'o')
 
+
+
+// Create 2 functions that calculate properties of a circle, using
+// the definitions here.
+// Create a function called calcCircumference:
+// • Pass the radius to the function.
+// • Calculate the circumference based on the radius, and output
+// "The circumference is NN".
+// Create a function called calcArea:
+// • Pass the radius to the function.
+// • Calculate the area based on the radius, and output "The area
+// is NN".
+
+function calcir(){
+
+
+
+}
+
+
+function calArea(){
+
+
+
+}
+
